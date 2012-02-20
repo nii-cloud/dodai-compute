7376800a8da33c4e711fbb0e70e55b63ec19e026
Xiaohua Guan <guanxiaohua2k6@gmail.com>
2012-02-20 19:35:42 +0900

    Remove save method call. Ignore errors during clear outer port association setting.

--- nova/virt/dodai/ofc_utils.py
+++ nova/virt/dodai/ofc_utils.py
@@ -12,7 +12,6 @@ def update_for_run_instance(service_url, region_name, server_port1, server_port2
 
     client.service.setServerPort(dpid1, server_port1, region_name)
     client.service.setServerPort(dpid2, server_port2, region_name)
-    client.service.save()
 
 def update_for_terminate_instance(service_url, region_name, server_port1, server_port2, dpid1, dpid2, vlan_id):
     client = Client(service_url + "?wsdl")
@@ -45,7 +44,6 @@ def create_region(service_url, region_name, vlan_id):
         switches = db.switch_get_all(None)
         for switch in switches:
             client.service.setOuterPortAssociationSetting(switch["dpid"], switch["outer_port"], vlan_id, 65535, region_name)
-        client.service.save()
     except:
         client.service.destroyRegion(region_name)
         raise exception.OFCRegionSettingOuterPortAssocFailed(region_name=region_name, vlan_id=vlan_id)
@@ -53,11 +51,14 @@ def create_region(service_url, region_name, vlan_id):
 def remove_region(service_url, region_name, vlan_id):
     client = Client(service_url + "?wsdl")
 
-    switches = db.switch_get_all(None)
-    for switch in switches:
-        client.service.clearOuterPortAssociationSetting(switch["dpid"], switch["outer_port"], vlan_id)
+    try:
+        switches = db.switch_get_all(None)
+        for switch in switches:
+            client.service.clearOuterPortAssociationSetting(switch["dpid"], switch["outer_port"], vlan_id)
+    except:
+        pass
+
     client.service.destroyRegion(region_name)
-    client.service.save()
 
 def has_region(service_url, region_name):
     client = Client(service_url + "?wsdl")
